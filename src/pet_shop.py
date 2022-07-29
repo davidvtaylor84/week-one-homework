8895043e560169313508d54210f60ef037f48aa8
David Valdez Taylor <davidvaldeztaylor@gmail.com>
2022-07-29 16:18:22 +0100

    first three functions

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,1 +1,16 @@
 # WRITE YOUR FUNCTIONS HERE
+
+def get_pet_shop_name(shop_name):
+    return shop_name["name"]
+
+def get_total_cash(cash):
+    return cash["admin"]["total_cash"]
+
+def add_or_remove_cash(total_cash, money):
+    total_cash["admin"]["total_cash"] += money
+    return total_cash
+    
+
+
+
+ 
